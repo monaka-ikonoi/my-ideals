6678c94818aa3f70cbcf37dacd1f34703b11783c
Monaka <monaka.ikonoi@gmail.com>
2026-01-05 17:27:24 +0800

    Initial commit
    Signed-off-by: Monaka <monaka.ikonoi@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">
+        My Ideals - Idol Namashashin Tracking App
+      </h1>
+    </div>
+  );
+}
