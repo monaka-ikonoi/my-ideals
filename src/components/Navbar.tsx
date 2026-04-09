99774d398c18983abe82af73777682a8ca4517f5
Monaka <monaka.ikonoi@gmail.com>
2026-04-09 22:43:03 +0800

    ui: Navbar: Fix typo
    Signed-off-by: Monaka <monaka.ikonoi@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export function Navbar() {
       <div className="mx-auto px-4 lg:max-w-[1024px] 2xl:max-w-[1664px]">
         <div className="flex h-14 items-center justify-between">
           {/* Left: Title */}
-          <div className="items-centerm flex cursor-pointer" onClick={scrollPageToTop}>
+          <div className="flex cursor-pointer items-center" onClick={scrollPageToTop}>
             <h1 className="text-lg font-bold text-gray-900">
               <span className="sm:hidden">
                 {DEV_MODE && <span className="mr-1">[DEV]</span>}
